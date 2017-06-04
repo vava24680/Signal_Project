c5277d860529c3ba27b718b7896681749da47e4c
vava24680 <9203700@gmail.com>
2017-06-04 22:33:44 +0800

    Edit some part

--- Reference_Code.py
+++ Reference_Code.py
@@ -6,33 +6,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
 x=np.array(range(200))
+
+
+# In[5]:
+
 print(x)
+
+
+# In[7]:
+
 y=np.sin(x)
 print(y)
-cv=open("sub017_baseline_BloodPulse.csv",'r')
 
-
-# In[20]:
+cv=open("./data/physiology/sub021_baseline_C1.csv",'r')
 
 txt=cv.read()
 
 
-# In[21]:
+#print(txt)
 
-print(txt)
-
-
-# In[22]:
 
 freq=np.fft.fft(y)
 
 
-# In[23]:
-
-print(freq)
-
-
-# In[24]:
+#print(freq)
 
 mag=np.absolute(freq)
 
@@ -50,47 +47,33 @@ dt=txt.split('\n')
 #    train[i]=train[i].split(',')
 #for j in range(len(test)):
 #    test[j]=test[j].split(',')
-print(len(dt[2]) )
+#print(len(dt[2]) )
 
-
-# In[34]:
 
 d0=dt[0].split(",")
-print(d0)
+#print(d0)
 
-
-# In[37]:
 
 npd0=np.array(d0[1:])
 #npd0E1=npd0[1:]
-print(npd0)
+#print(npd0)
 
-
-# In[38]:
-
+print(len(npd0))
 fd0=npd0.astype(float)
 
 
-# In[39]:
-
-print(fd0)
-
-
-# In[40]:
+#print(fd0)
+print(len(fd0))
 
 fre=np.fft.fft(fd0)
-
-
-# In[43]:
+print(len(fre))
 
 x=np.array(range(1000))
-print(x)
+#print(x)
 
 
-# In[47]:
-
-plt.plot(x,np.absolute(fre))
-plt.show()
+#plt.plot(x,np.absolute(fre))
+#plt.show()
 
 
 # In[ ]:
