1c14d96bdd66ed2efe4ae3e35bb7a08fd541096e
vava24680 <9203700@gmail.com>
2017-06-05 14:50:40 +0800

    2_3

--- 2_3.py
+++ 2_3.py
@@ -14,25 +14,25 @@ end_time=datetime.datetime.strptime("13:41:40","%H:%M:%S")
 total_sec=0
 start_index=0;
 for i in range(0,len_of_tsdata):
-    temp_time=datetime.datetime.strptime(ts_data[i][0]," %H:%M:%S.")
-    if(start_time<temp_time):
-        total_sec+=1
-        break;
-    else:
-        start_index=i+1
+	temp_time=datetime.datetime.strptime(ts_data[i][0]," %H:%M:%S.")
+	if(start_time<=temp_time):
+		total_sec+=1
+		break;
+	else:
+		start_index=i+1
 final_data = np.array(ts_data[start_index][1:])
-print(start_index)
 
 for i in range(start_index+1,len_of_tsdata):
-    temp_time=datetime.datetime.strptime(ts_data[i][0]," %H:%M:%S.")
-    if(start_time<=temp_time and temp_time<=end_time):
-        total_sec+=1
-        final_data = np.append(final_data,ts_data[i][1:])
+	temp_time=datetime.datetime.strptime(ts_data[i][0]," %H:%M:%S.")
+	if(start_time<=temp_time and temp_time<=end_time):
+		total_sec+=1
+		final_data = np.append(final_data,ts_data[i][1:])
+	else:
+		break
 print(total_sec)
 print(len(final_data))
 
 final_data = final_data.astype(float)
 time_asix = np.arange(0,total_sec,0.001,float)
-print(len(time_asix))
 plt.plot(time_asix,np.absolute(final_data))
 plt.show()
