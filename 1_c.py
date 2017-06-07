fc73dfec09ff8c7e1df6cde4c632253ff212f8be
vava24680 <9203700@gmail.com>
2017-06-07 20:19:02 +0800

    Rename

---
+++ 1_c.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+fptr=open("./data/physiology/sub021_baseline_C3.csv",'r')
+#Change file name to change read data
+raw_data = fptr.read()
+ts_data = raw_data.split('\n')#time_split data
+len_of_tsdata = len(ts_data)-1
+print(len_of_tsdata)
+#print(len_of_tsdata)
+for i in range(len(ts_data)):
+    ts_data[i]=ts_data[i].split(',')
+
+final_data = np.append(ts_data[0][1:],ts_data[1][1:])
+for i in range(2,len_of_tsdata):
+    final_data = np.append(final_data,ts_data[i][1:])
+final_data = final_data.astype(float)
+time_axix = np.arange(0,len_of_tsdata,0.001,float)
+plt.plot(time_axix,np.absolute(final_data))
+plt.show()
